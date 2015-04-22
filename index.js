fd025563a84adb18bb580305f12595525a0da1f2
Marco Godínez <markotom@gmail.com>
2015-04-21 19:40:02 -0500

    Redefining property names, besides it was added public functions

--- index.js
+++ index.js
@@ -3,16 +3,21 @@
 var CacheHandler = function (config) {
   config = config || {};
 
-  if (!config.config) { throw new Error('Missing mandatory config property'); }
+  if (!config.cacheables) { throw new Error('Missing mandatory cacheables property'); }
   if (!config.engine) { throw new Error('Missing mandatory engine property'); }
 
-  this.config = config.config;
+  this.cacheables = config.cacheables;
   this.engine = config.engine;
+  this.ttl = config.ttl || 3600;
 
-  if (typeof this.config === 'string') {
-    this._configFile = config.config;
-    this.config = require(this.config);
+  if (typeof this.cacheables === 'string') {
+    this._cacheablesFile = config.cacheables;
+    this.cacheables = require(this.cacheables);
   };
 };
+
+// redis.hmset(obj, _.pick(obj, propNames))
+CacheHandler.prototype.get = function () {};
+CacheHandler.prototype.set = function () {};
 
 module.exports = CacheHandler;
--- test/cache.js
+++ test/cache.js
@@ -4,16 +4,20 @@ var assert = require('assert');
 var redis = require('redis');
 var CacheHandler = require('../index');
 
-var client = redis.createClient();
-var configFile = './test/config.json';
-var config = {
+var client = redis.createClient(6379, 'localhost');
+var cacheablesFile = './test/cacheables.json';
+var cacheables = {
   prop: {
     subprop1: true,
     subprop2: false,
   }
 };
 
-var cacheHandler = new CacheHandler({ config: config, engine: client });
+var cacheHandler = new CacheHandler({
+  cacheables: cacheables,
+  engine: client,
+  ttl: 3600
+});
 
 describe('Cache Handler', function () {
   it('should be a function', function () {
@@ -24,36 +28,46 @@ describe('Cache Handler', function () {
     assert(cacheHandler instanceof CacheHandler);
   });
 
-  it('should be initialized without mandatory `config` property', function () {
+  it('should be initialized without mandatory `cacheables` property', function () {
     try {
       var _cacheHandler = new CacheHandler({ engine: client });
     } catch (err) {
       assert(err instanceof Error, 'error is not an instance of Error');
-      assert.equal(err, 'Error: Missing mandatory config property');
+      assert.equal(err, 'Error: Missing mandatory cacheables property');
     }
   });
 
   it('should be initialized without mandatory `engine` property', function () {
     try {
-      var _cacheHandler = new CacheHandler({ config: config });
+      var _cacheHandler = new CacheHandler({ cacheables: cacheables });
     } catch (err) {
       assert(err instanceof Error, 'error is not an instance of Error');
       assert.equal(err, 'Error: Missing mandatory engine property');
     }
   });
 
-  it('should be initialized with config as object or path', function () {
+  it('should be initialized with cacheables as object or path', function () {
     var _cacheHandler;
 
-    _cacheHandler = new CacheHandler({ config: config, engine: client });
-    assert.equal(typeof _cacheHandler.config, 'object');
+    _cacheHandler = new CacheHandler({ cacheables: cacheables, engine: client });
+    assert.equal(typeof _cacheHandler.cacheables, 'object');
 
-    _cacheHandler = new CacheHandler({ config: configFile, engine: client });
-    assert.equal(typeof _cacheHandler.config, 'object');
-    assert.equal(_cacheHandler._configFile, configFile);
+    _cacheHandler = new CacheHandler({ cacheables: cacheablesFile, engine: client });
+    assert.equal(typeof _cacheHandler.cacheables, 'object');
+    assert.equal(_cacheHandler._cacheablesFile, cacheablesFile);
   });
 
-  it('should have a config property correctly', function () {
-    assert(typeof cacheHandler.config, 'object');
+  it('should have this properties correctly', function () {
+    assert(cacheHandler.ttl);
+    assert.equal(typeof cacheHandler.ttl, 'number');
+    assert.equal(cacheHandler.ttl, 3600);
   });
+
+  it('should have this public functions', function () {
+    assert(cacheHandler.get);
+    assert(cacheHandler.set);
+    assert(cacheHandler.verify);
+  });
+
+  it('should have this private functions', function () {});
 });
